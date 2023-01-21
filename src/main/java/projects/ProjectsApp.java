5347a3de2065173dbb7fea72222210d937425ce1
Lori <lcenos5@gmail.com>
2023-01-21 11:23:04 -0600

    week 9

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,12 +1,45 @@
 package projects;
 
-import projects.dao.DbConnection;
+import java.util.Objects;
 
 public class ProjectsApp {
 
 	public static void main(String[] args) {
-		DbConnection.getConnection();
+		
+		// @formatter:off
+		private List<String> operations = List.of("1) Add a project");
+		// @formatter:on
+		
+		private Scanner scanner = new Scanner(System.in);
+		
+		new ProjectsApp().processUserSelections();
+	} //end main method
 
+	private void processUserSelections() {
+		boolean done = false;
+		
+		while(!done) {
+		  try {
+			  int selection = getUserSelection();
+		  }
+		  catch(Exception e) {
+			  System.out.println("\nError: " + e + " Try again.");
+		  }
+		}
+	
 	}
 
-}
+	private int getUserSelection() {
+		printOperations();
+		
+		Integer input = getIntInput("Enter a menu selection");
+		return Objects.isNull(input) ? -1 : input;
+	}
+
+	private void printOperations() {
+		System.out.println("\nThese are the available selections.  Press the enter key to quit:");
+		  operations.forEach(line ->)
+		
+	}
+
+}//end class
--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS materials;
 DROP TABLE IF EXISTS step;
 DROP TABLE IF EXISTS project_category;
-DROP TABLE IF EXISTS project;
 DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS project;
 
 CREATE TABLE project (
-project_id int AUTO_INCREMENTS NOT NULL,
+project_id int AUTO_INCREMENT NOT NULL,
 project_name varchar(128) NOT NULL,
 estimate_hours decimal(7,2),
 actual_hours decimal (7,2),
@@ -15,7 +15,7 @@ PRIMARY KEY (project_id)
 );
 
 CREATE TABLE category (  
-category_id int AUTO_INCREMENTS NOT NULL,
+category_id int AUTO_INCREMENT NOT NULL,
 category_name varchar(128) NOT NULL,
 PRIMARY KEY (category_id)
 );
@@ -29,7 +29,7 @@ UNIQUE KEY (project_id, category_id)
 );
 
 CREATE TABLE step (
-step_id int AUTO_INCREMENTS NOT NULL,
+step_id int AUTO_INCREMENT NOT NULL,
 project_id int NOT NULL,
 step_text TEXT NOT NULL,
 step_order int NOT NULL,
@@ -38,7 +38,7 @@ FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
 
 CREATE TABLE materials (
-material_id int AUTO_INCREMENTS NOT NULL,
+material_id int AUTO_INCREMENT NOT NULL,
 project_id int NOT NULL,
 material_name varchar(128) NOT NULL,
 num_required int,
