498ac3cc3be171875d01cfbebbf941b54ded6c86
Lori <lcenos5@gmail.com>
2023-02-04 21:09:49 -0600

    week 11

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 	
@@ -25,6 +26,21 @@ public class ProjectService {
     			
 
     }
+
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() 
+			+ "does not exist.");
+		}
+		
+	}
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+		}
+	}
+	
 	
 
 }
--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -28,6 +28,17 @@ FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
 UNIQUE KEY (project_id, category_id)
 );
 
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes) VALUES ('Door', 4, 3, 3, 'Used HD hangers');
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, 'Door hanger', 1);
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, 'Screws', 20,);
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Align hangers on opening side of door vertically on the wall');
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Screw hangers into frame', 2);
+INSERT INTO category (category_id, category_name) VALUES (1, 'Doors and Windows');
+INSERT INTO category (category_id, category_name) VALUES(2, 'Repairs');
+INSERT INTO category (category_id, category_name) VALUES (3, 'Gardening');
+INSERT INTO project_category (project_id, category_id) VALUES (1, 1);
+INSERT INTO project_category (project_id, category_id) VALUES (1, 2);
+
 CREATE TABLE step (
 step_id int AUTO_INCREMENT NOT NULL,
 project_id int NOT NULL,
